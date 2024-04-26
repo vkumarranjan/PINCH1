fd916cfb0646e7d3cc85aa0071b77372c69b9f6b
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-04-26 11:53:48 +0530

    Initial Commit

---
+++ PINCH/PINCHApp.swift
@@ -0,0 +1,17 @@
+//
+//  PINCHApp.swift
+//  PINCH
+//
+//  Created by vinay Kumar ranjan on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct PINCHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
